721e039113ea4fb3040c36cc93419dd08a1a5bfd
MateoEmilio1 <mateoyastor60@gmail.com>
2024-03-15 21:31:34 -0300

    noves data provider added

--- index.js
+++ index.js
@@ -3,11 +3,39 @@ const app = express();
 const port = process.env.PORT || 5001;
 const Moralis = require("moralis").default;
 const cors = require("cors");
+const axios = require("axios");
+const Web3 = require("web3");
 
 require("dotenv").config({ path: ".env" });
 
 app.use(cors());
 app.use(express.json());
+
+// NOVES
+
+const NOVES_API_KEY = process.env.NOVES_API_KEY;
+
+// Crea una nueva ruta en tu aplicación Express para manejar las solicitudes a /evm/chains
+app.get('/evm/chains', async (req, res) => {
+  try {
+    const options = {
+      method: 'GET',
+      url: 'https://foresight.noves.fi/evm/chains',
+      headers: { accept: 'application/json', apiKey: NOVES_API_KEY },
+    };
+
+    // Realiza la solicitud a la API de NOVES
+    const response = await axios.request(options);
+    const data = response.data; // Obtiene la data de la respuesta
+
+    return res.status(200).json(data); // Devuelve la data obtenida como respuesta
+  } catch (error) {
+    console.error(error);
+    return res.status(400).json({ error: 'Something went wrong' });
+  }
+});
+
+//MORALIS
 
 const MORALIS_API_KEY = process.env.MORALIS_API_KEY;
 
