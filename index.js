6f5465ef079d558934524c57f4718c2c7e137b95
Tute <mateoyastor60@gmail.com>
2024-04-09 12:47:59 -0300

    testing getTxnbyHash

--- index.js
+++ index.js
@@ -10,7 +10,9 @@ app.use(cors());
 app.use(express.json());
 
 const MORALIS_API_KEY = process.env.MORALIS_API_KEY;
+const INFURA_API_KEY = process.env.INFURA_API_KEY;
 
+/* GET ETH PRICE */
 app.get("/getethprice", async (req, res) => {
   try {
     const response = await Moralis.EvmApi.token.getTokenPrice({
@@ -25,6 +27,8 @@ app.get("/getethprice", async (req, res) => {
   }
 });
 
+
+/* ADDRESS */
 app.get("/address", async (req, res) => {
   try {
     const { query } = req;
@@ -48,4 +52,39 @@ Moralis.start({
   app.listen(port, () => {
     console.log(`Listening for API Calls`);
   });
+});
+
+/* GET TRANSACTION BY HASH */
+
+
+app.post("/getTransactionByHash", async (req, res) => {
+  const { hash } = req.body; // El hash de la transacción se espera en el cuerpo de la solicitud
+  
+  const provider = `https://mainnet.infura.io/v3/${INFURA_API_KEY}`;
+
+  const data = {
+    jsonrpc: '2.0',
+    method: 'eth_getTransactionByHash',
+    params: [hash],
+    id: 1,
+  };
+
+  const requestOptions = {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(data),
+  };
+
+  try {
+    const response = await fetch(provider, requestOptions);
+    const responseData = await response.json();
+    res.status(200).json(responseData);
+  } catch (error) {
+    console.error('There was a problem with your fetch operation:', error);
+    res.status(500).json({ error: 'Something went wrong with the fetch operation' });
+  }
+});
+
+app.listen(5001, () => {
+  console.log(`Server running on port 5001`);
 });
